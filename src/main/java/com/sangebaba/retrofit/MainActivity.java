ffc2ee39bce5087f9dbca27d1ad67122efec7cd5
zanyang <zanyang521@163.com>
2016-12-21 17:06:02 +0800

    RxPermissions 的简单了解

--- src/main/java/com/sangebaba/retrofit/MainActivity.java
+++ src/main/java/com/sangebaba/retrofit/MainActivity.java
@@ -4,16 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.jakewharton.rxbinding.view.RxView;
 import com.sangebaba.base.BaseActivity;
 import com.sangebaba.util.DebugLog;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 import java.util.concurrent.TimeUnit;
+import java.util.jar.Manifest;
 
 import butterknife.Bind;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.functions.Action0;
 import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 
 public class MainActivity extends BaseActivity {
@@ -32,6 +39,35 @@ public class MainActivity extends BaseActivity {
 //
 //            }
 //        });
+
+        // TODO: 2016/12/20 rx 轮询定时操作
+        Observable.interval(2, 2, TimeUnit.SECONDS)
+                .subscribeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe(new Action0() {
+                    @Override
+                    public void call() {
+                        DebugLog.i("重启一个线程每隔两秒执行一次");
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(Long aLong) {
+                        DebugLog.i("每隔两秒执行一次");
+
+                    }
+                });
+
+        Observable.timer(2, TimeUnit.SECONDS).subscribe(new Action1<Long>() {
+            @Override
+            public void call(Long aLong) {
+                DebugLog.i("两秒钟后去执行某些操作");
+            }
+        });
+
+        // TODO: 2016/12/20 rxpermissions
+        final RxPermissions rxPermissions = new RxPermissions(this);
+
     }
 
     @Override
